cedb6fbf128d53cb40731792fa3bd432e0598e69
Madison Randle <madisonrandle25@gmail.com>
2020-08-29 16:46:32 -0500

    Update App test to check Nav component render

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders hi', () => {
+test('renders App', () => {
   const { getByText } = render(<App />);
-  const nav = getByText('Nav');
-  expect(nav).toBeInTheDocument();
+  const navComponent = getByText('Nav');
+  expect(navComponent).toBeInTheDocument();
 });
